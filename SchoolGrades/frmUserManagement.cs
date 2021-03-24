a95582fad810cdf4a916e86a5a2756970d3b0623
Gabriele MONTI <gamondue@gmail.com>
2021-03-24 09:20:22 +0100

    Implementation of DataLayer GetAllUsers.

---
+++ SchoolGrades/frmUserManagement.cs
@@ -0,0 +1,49 @@
+﻿using SchoolGrades.DbClasses;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SchoolGrades
+{
+    public partial class frmUserManagement : Form
+    {
+        User currentUser;
+        SchoolGrades.BusinessLayer.BusinessLayer bl = new BusinessLayer.BusinessLayer(); 
+
+        public frmUserManagement()
+        {
+            InitializeComponent();
+
+
+        }
+        private void frmUserManagement_Load(object sender, EventArgs e)
+        {
+            // starts when the calling program calls Show()
+ 
+        }
+        private void lstUser_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lstUser.DataSource = bl.GetAllUsers();
+            currentUser = (User)sender;
+            FromClassToUi(); 
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            FromUiToClass(); 
+            bl.UpdateUser(currentUser); 
+        }
+        /// <summary>
+        /// Read the Class User from UI
+        /// </summary>
+        private void FromUiToClass()
+        {
+            currentUser.LastName = txtLastName.Text; 
+            throw new NotImplementedException();
+        }
+    }
+}
