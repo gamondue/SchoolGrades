643af356928241f28d867ac5be1a7970d9db83bc
Gabriele MONTI <gamondue@gmail.com>
2021-03-24 09:20:22 +0100

    Implementation of DataLayer GetAllUsers.

--- DataLayer/DataLayer.cs
+++ DataLayer/DataLayer.cs
@@ -1,5 +1,6 @@
 ﻿using SchoolGrades.DbClasses;
 using System;
+using System.Collections.Generic;
 using System.Data.Common;
 using System.Data.SQLite;
 
@@ -24,6 +25,28 @@ namespace SchoolGrades.DataLayer
             }
             dbName = Commons.PathAndFileDatabase;
         }
+        internal List<User> GetAllUsers()
+        {
+            List<User> l = new List<User>();
+            using (DbConnection conn = Connect())
+            {
+                DbCommand cmd = conn.CreateCommand();
+                string query = "SELECT *" +
+                    " FROM Users";
+                cmd = new SQLiteCommand(query);
+                cmd.Connection = conn;
+                DbDataReader dRead = cmd.ExecuteReader();
+                while (dRead.Read())
+                {
+                    User u = GetUserFromRow(dRead);
+                    l.Add(u);
+                }
+                dRead.Dispose();
+                cmd.Dispose();
+            }
+            return l;
+        }
+
         public DataLayer(string PathAndFile)
         {
             if (!System.IO.File.Exists(PathAndFile))
