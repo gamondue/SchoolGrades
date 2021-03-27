9c85a19b1ad25e0becd55b18e95c7a7a78e8994d
robertop03 <roberto.pisu03@gmail.com>
2021-03-27 11:47:50 +0100

    Possibilità di creare un nuovo utente o di modificare un utente già esistente.

--- SchoolGrades/frmUsersManagement.Designer.cs
+++ SchoolGrades/frmUsersManagement.Designer.cs
@@ -30,33 +30,234 @@ namespace SchoolGrades
         private void InitializeComponent()
         {
             this.lstUsers = new System.Windows.Forms.ListBox();
+            this.grbUpdates = new System.Windows.Forms.GroupBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.rtxtPassword = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rtxtDescription = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnModify = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.grbUpdates.SuspendLayout();
             this.SuspendLayout();
             // 
-            // listBox1
+            // lstUsers
             // 
             this.lstUsers.FormattingEnabled = true;
             this.lstUsers.ItemHeight = 20;
-            this.lstUsers.Location = new System.Drawing.Point(29, 33);
-            this.lstUsers.Name = "listBox1";
-            this.lstUsers.Size = new System.Drawing.Size(150, 484);
+            this.lstUsers.Location = new System.Drawing.Point(580, 47);
+            this.lstUsers.Name = "lstUsers";
+            this.lstUsers.Size = new System.Drawing.Size(290, 484);
             this.lstUsers.TabIndex = 0;
             this.lstUsers.SelectedIndexChanged += new System.EventHandler(this.lstUsers_SelectedIndexChanged);
+            // 
+            // grbUpdates
+            // 
+            this.grbUpdates.Controls.Add(this.btnCreate);
+            this.grbUpdates.Controls.Add(this.btnUpdate);
+            this.grbUpdates.Controls.Add(this.rtxtPassword);
+            this.grbUpdates.Controls.Add(this.label6);
+            this.grbUpdates.Controls.Add(this.txtEmail);
+            this.grbUpdates.Controls.Add(this.txtFirstName);
+            this.grbUpdates.Controls.Add(this.label5);
+            this.grbUpdates.Controls.Add(this.label4);
+            this.grbUpdates.Controls.Add(this.rtxtDescription);
+            this.grbUpdates.Controls.Add(this.label3);
+            this.grbUpdates.Controls.Add(this.txtLastName);
+            this.grbUpdates.Controls.Add(this.label2);
+            this.grbUpdates.Controls.Add(this.txtUsername);
+            this.grbUpdates.Controls.Add(this.label1);
+            this.grbUpdates.Location = new System.Drawing.Point(21, 47);
+            this.grbUpdates.Name = "grbUpdates";
+            this.grbUpdates.Size = new System.Drawing.Size(451, 457);
+            this.grbUpdates.TabIndex = 4;
+            this.grbUpdates.TabStop = false;
+            this.grbUpdates.Text = "Updates";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(6, 410);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(94, 29);
+            this.btnCreate.TabIndex = 14;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(338, 410);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(94, 29);
+            this.btnUpdate.TabIndex = 13;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // rtxtPassword
+            // 
+            this.rtxtPassword.Location = new System.Drawing.Point(120, 344);
+            this.rtxtPassword.Name = "rtxtPassword";
+            this.rtxtPassword.Size = new System.Drawing.Size(205, 29);
+            this.rtxtPassword.TabIndex = 12;
+            this.rtxtPassword.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(24, 344);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(84, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Password =";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(119, 258);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(206, 27);
+            this.txtEmail.TabIndex = 10;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(120, 296);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(205, 27);
+            this.txtFirstName.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 299);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "First name =";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 258);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Email =";
+            // 
+            // rtxtDescription
+            // 
+            this.rtxtDescription.Location = new System.Drawing.Point(119, 118);
+            this.rtxtDescription.Name = "rtxtDescription";
+            this.rtxtDescription.Size = new System.Drawing.Size(242, 113);
+            this.rtxtDescription.TabIndex = 6;
+            this.rtxtDescription.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Description =";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(120, 73);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(125, 27);
+            this.txtLastName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Last name =";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(119, 30);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(125, 27);
+            this.txtUsername.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username =";
+            // 
+            // btnModify
+            // 
+            this.btnModify.Location = new System.Drawing.Point(378, 13);
+            this.btnModify.Name = "btnModify";
+            this.btnModify.Size = new System.Drawing.Size(94, 29);
+            this.btnModify.TabIndex = 3;
+            this.btnModify.Text = "Modify";
+            this.btnModify.UseVisualStyleBackColor = true;
+            this.btnModify.Click += new System.EventHandler(this.btnModify_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(580, 17);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 20);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Users:";
             // 
             // frmUsersManagement
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(980, 551);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.grbUpdates);
+            this.Controls.Add(this.btnModify);
             this.Controls.Add(this.lstUsers);
             this.Name = "frmUsersManagement";
             this.Text = "frmUsersManagement";
             this.Load += new System.EventHandler(this.frmUsersManagement_Load);
+            this.grbUpdates.ResumeLayout(false);
+            this.grbUpdates.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ListBox lstUsers;
+        private System.Windows.Forms.GroupBox grbUpdates;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.RichTextBox rtxtPassword;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox rtxtDescription;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnModify;
+        private System.Windows.Forms.Label label7;
     }
 }
--- SchoolGrades/frmUsersManagement.cs
+++ SchoolGrades/frmUsersManagement.cs
@@ -9,11 +9,12 @@ namespace SchoolGrades
     {
         BusinessLayer.BusinessLayer bl = new BusinessLayer.BusinessLayer();
 
-        List<User> listOfAllUsers; 
-
+        List<User> listOfAllUsers;
+        User currentUser;
         public frmUsersManagement()
         {
             InitializeComponent();
+            grbUpdates.Visible = false;
         }
 
         private void frmUsersManagement_Load(object sender, EventArgs e)
@@ -24,7 +25,74 @@ namespace SchoolGrades
 
         private void lstUsers_SelectedIndexChanged(object sender, EventArgs e)
         {
-            User currentUser = (User)(listOfAllUsers[lstUsers.SelectedIndex]); 
+            currentUser = (listOfAllUsers[lstUsers.SelectedIndex]); 
+        }
+
+        private void btnModify_Click(object sender, EventArgs e)
+        {
+            if (!grbUpdates.Visible)
+                grbUpdates.Visible = true;
+            else
+                grbUpdates.Visible = false;
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtUsername.Text.Length < 1)
+                    throw new Exception("The new utent must contains an username!");
+                if (rtxtPassword.Text.Length < 6)
+                    throw new Exception("Password must contains min 6 characters.");
+                User newUser = new User(txtUsername.Text, rtxtPassword.Text);
+
+                if (txtLastName.Text.Length > 1)
+                    newUser.LastName = txtLastName.Text;
+                if (rtxtDescription.Text.Length > 1)
+                    newUser.Description = rtxtDescription.Text;
+                if (txtEmail.Text.Length > 1)
+                    newUser.Email = txtEmail.Text;
+                if (txtFirstName.Text.Length > 1)
+                    newUser.FirstName = txtFirstName.Text;
+
+                bl.CreateUser(newUser);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("There is problem!" + ex.Message,
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Prima aggiorniamo proprietà dello User, e poi tramite il metodo della classe Business layer
+        // "Update User" aggiorna il database.
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtUsername.Text.Length > 1)
+                    currentUser.Username = txtUsername.Text;
+                if (txtLastName.Text.Length > 1)
+                    currentUser.LastName = txtLastName.Text;
+                if (rtxtDescription.Text.Length > 1)
+                    currentUser.Username = rtxtDescription.Text;
+                if (txtEmail.Text.Length > 1)
+                    currentUser.Email = txtEmail.Text;
+                if (txtFirstName.Text.Length > 1)
+                    currentUser.FirstName = txtFirstName.Text;
+                if (rtxtPassword.Text.Length < 6)
+                    throw new Exception("Password must contains min 6 characters.");
+
+                currentUser.Password = rtxtPassword.Text;
+
+                bl.ChangePassword(currentUser);
+                bl.UpdateUser(currentUser);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("There is problem!" + ex.Message,
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
