01f95b9f64001d2c8c87e16e06dec14cb5bde4a5
Francesco Foschi <79740595+Francesco08-bit@users.noreply.github.com>
2021-03-27 09:46:00 +0100

    Nuova Versione 2.0

--- BusinessLayer/BusinessLayer.cs
+++ BusinessLayer/BusinessLayer.cs
@@ -71,6 +71,20 @@ namespace SchoolGrades.BusinessLayer
             // !!!! TODO !!!!
             return null;
         }
+
+        static string calcoloSHA256(string rawData)
+        {
+            using (SHA256 hash = SHA256.Create())
+            {
+                byte[] bytes = hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
         #endregion
     }
 }
