8fc576f39974f982b261df17f8540470da6e1802
yosbotnet <79740870+yosbotnet@users.noreply.github.com>
2021-03-27 09:00:34 +0100

    Update frmUsersManagement.Designer.cs

--- SchoolGrades/frmUsersManagement.Designer.cs
+++ SchoolGrades/frmUsersManagement.Designer.cs
@@ -30,33 +30,91 @@ namespace SchoolGrades
         private void InitializeComponent()
         {
             this.lstUsers = new System.Windows.Forms.ListBox();
+            this.txtUtente = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.btnCambiaCredenziali = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // listBox1
+            // lstUsers
             // 
             this.lstUsers.FormattingEnabled = true;
-            this.lstUsers.ItemHeight = 20;
-            this.lstUsers.Location = new System.Drawing.Point(29, 33);
-            this.lstUsers.Name = "listBox1";
-            this.lstUsers.Size = new System.Drawing.Size(150, 484);
+            this.lstUsers.ItemHeight = 15;
+            this.lstUsers.Location = new System.Drawing.Point(25, 25);
+            this.lstUsers.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lstUsers.Name = "lstUsers";
+            this.lstUsers.Size = new System.Drawing.Size(132, 364);
             this.lstUsers.TabIndex = 0;
             this.lstUsers.SelectedIndexChanged += new System.EventHandler(this.lstUsers_SelectedIndexChanged);
             // 
+            // txtUtente
+            // 
+            this.txtUtente.Location = new System.Drawing.Point(261, 25);
+            this.txtUtente.Name = "txtUtente";
+            this.txtUtente.Size = new System.Drawing.Size(228, 23);
+            this.txtUtente.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(164, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "utente:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(164, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "password:";
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(261, 73);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(228, 23);
+            this.txtPass.TabIndex = 4;
+            // 
+            // btnCambiaCredenziali
+            // 
+            this.btnCambiaCredenziali.Location = new System.Drawing.Point(164, 115);
+            this.btnCambiaCredenziali.Name = "btnCambiaCredenziali";
+            this.btnCambiaCredenziali.Size = new System.Drawing.Size(325, 23);
+            this.btnCambiaCredenziali.TabIndex = 5;
+            this.btnCambiaCredenziali.Text = "cambia credenziali";
+            this.btnCambiaCredenziali.UseVisualStyleBackColor = true;
+            // 
             // frmUsersManagement
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(980, 551);
+            this.ClientSize = new System.Drawing.Size(858, 413);
+            this.Controls.Add(this.btnCambiaCredenziali);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUtente);
             this.Controls.Add(this.lstUsers);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "frmUsersManagement";
-            this.Text = "frmUsersManagement";
             this.Load += new System.EventHandler(this.frmUsersManagement_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ListBox lstUsers;
+        private System.Windows.Forms.TextBox txtUtente;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Button btnCambiaCredenziali;
     }
 }
