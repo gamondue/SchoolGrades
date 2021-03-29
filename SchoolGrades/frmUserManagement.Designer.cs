11bc55a0617136e87f0819b09022819fa4f9f2e9
Luca Casadei <casadeiluca30@gmail.com>
2021-03-29 21:23:01 +0200

    Completata l'interfaccia grafica del form UserManagement, gestito il cambio di utente cliccando sulla lista, caricamento delle informazioni aggiornate non riuscito dovuto ad un errore del database.

--- SchoolGrades/frmUserManagement.Designer.cs
+++ SchoolGrades/frmUserManagement.Designer.cs
@@ -30,28 +30,40 @@ namespace SchoolGrades
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.lstUser = new System.Windows.Forms.ListBox();
+            this.ListaUtenti = new System.Windows.Forms.ListBox();
             this.btnSave = new System.Windows.Forms.Button();
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.txtLastName = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.chkIsUserEnabled = new System.Windows.Forms.CheckBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // lstUser
+            // ListaUtenti
             // 
-            this.lstUser.ForeColor = System.Drawing.Color.DarkBlue;
-            this.lstUser.FormattingEnabled = true;
-            this.lstUser.ItemHeight = 20;
-            this.lstUser.Location = new System.Drawing.Point(12, 12);
-            this.lstUser.Name = "lstUser";
-            this.lstUser.Size = new System.Drawing.Size(120, 344);
-            this.lstUser.TabIndex = 0;
-            this.lstUser.SelectedIndexChanged += new System.EventHandler(this.lstUser_SelectedIndexChanged);
+            this.ListaUtenti.ForeColor = System.Drawing.Color.DarkBlue;
+            this.ListaUtenti.FormattingEnabled = true;
+            this.ListaUtenti.ItemHeight = 20;
+            this.ListaUtenti.Location = new System.Drawing.Point(569, 16);
+            this.ListaUtenti.Name = "ListaUtenti";
+            this.ListaUtenti.Size = new System.Drawing.Size(416, 344);
+            this.ListaUtenti.TabIndex = 0;
+            this.ListaUtenti.SelectedIndexChanged += new System.EventHandler(this.lstUser_SelectedIndexChanged);
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(151, 310);
+            this.btnSave.Location = new System.Drawing.Point(44, 314);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(75, 46);
             this.btnSave.TabIndex = 1;
@@ -67,7 +79,7 @@ namespace SchoolGrades
             // 
             // txtLastName
             // 
-            this.txtLastName.Location = new System.Drawing.Point(151, 36);
+            this.txtLastName.Location = new System.Drawing.Point(44, 39);
             this.txtLastName.Name = "txtLastName";
             this.txtLastName.ReadOnly = true;
             this.txtLastName.Size = new System.Drawing.Size(100, 27);
@@ -76,32 +88,143 @@ namespace SchoolGrades
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(151, 13);
+            this.label1.Location = new System.Drawing.Point(44, 16);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(74, 20);
             this.label1.TabIndex = 3;
             this.label1.Text = "Cognome";
             // 
-            // button1
+            // label2
             // 
-            this.button1.Location = new System.Drawing.Point(292, 180);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 46);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Salva";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(181, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nome";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(181, 39);
+            this.txtName.Name = "txtName";
+            this.txtName.ReadOnly = true;
+            this.txtName.Size = new System.Drawing.Size(100, 27);
+            this.txtName.TabIndex = 5;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(313, 39);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(205, 27);
+            this.txtEmail.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(313, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Username";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(44, 104);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(474, 27);
+            this.txtUsername.TabIndex = 9;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(43, 157);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(474, 27);
+            this.txtDescription.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(43, 134);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Descrizione";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(43, 187);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 20);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Password";
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(44, 210);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(237, 27);
+            this.txtPass.TabIndex = 13;
+            // 
+            // chkIsUserEnabled
+            // 
+            this.chkIsUserEnabled.AutoSize = true;
+            this.chkIsUserEnabled.Location = new System.Drawing.Point(313, 210);
+            this.chkIsUserEnabled.Name = "chkIsUserEnabled";
+            this.chkIsUserEnabled.Size = new System.Drawing.Size(134, 24);
+            this.chkIsUserEnabled.TabIndex = 14;
+            this.chkIsUserEnabled.Text = "Utente Abilitato";
+            this.chkIsUserEnabled.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(44, 240);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(24, 20);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "ID";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(43, 263);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(151, 27);
+            this.txtID.TabIndex = 16;
             // 
             // frmUserManagement
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.PowderBlue;
-            this.ClientSize = new System.Drawing.Size(800, 372);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1030, 372);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.chkIsUserEnabled);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtLastName);
             this.Controls.Add(this.btnSave);
-            this.Controls.Add(this.lstUser);
+            this.Controls.Add(this.ListaUtenti);
             this.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.ForeColor = System.Drawing.Color.DarkBlue;
             this.Name = "frmUserManagement";
@@ -114,11 +237,23 @@ namespace SchoolGrades
 
         #endregion
 
-        private System.Windows.Forms.ListBox lstUser;
+        private System.Windows.Forms.ListBox ListaUtenti;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.ImageList imageList1;
         private System.Windows.Forms.TextBox txtLastName;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.CheckBox chkIsUserEnabled;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtID;
     }
 }
--- SchoolGrades/frmUserManagement.cs
+++ SchoolGrades/frmUserManagement.cs
@@ -11,25 +11,43 @@ namespace SchoolGrades
 {
     public partial class frmUserManagement : Form
     {
+        List<User> listaUtenti;
         User currentUser;
         SchoolGrades.BusinessLayer.BusinessLayer bl = new BusinessLayer.BusinessLayer(); 
 
         public frmUserManagement()
         {
             InitializeComponent();
-
-
         }
         private void frmUserManagement_Load(object sender, EventArgs e)
         {
             // starts when the calling program calls Show()
- 
+            listaUtenti = bl.GetAllUsers();
+            ListaUtenti.DataSource = listaUtenti;
         }
         private void lstUser_SelectedIndexChanged(object sender, EventArgs e)
         {
-            lstUser.DataSource = bl.GetAllUsers();
-            currentUser = (User)sender;
-            FromClassToUi(); 
+            currentUser = listaUtenti[ListaUtenti.SelectedIndex];
+            FromClassToUi();
+        }
+
+        private void FromClassToUi()
+        {
+            txtEmail.Text = currentUser.Email;
+            txtDescription.Text = currentUser.Description;
+            txtPass.Text = currentUser.Password;
+            txtName.Text = currentUser.FirstName;
+            txtLastName.Text = currentUser.LastName;
+            txtUsername.Text = currentUser.Username;
+            if ((bool)currentUser.IsEnabled)
+            {
+                chkIsUserEnabled.Checked = true;
+            }
+            else
+            {
+                chkIsUserEnabled.Checked = false;
+            }
+            txtID.Text = currentUser.IdUserCategory.ToString();
         }
 
         private void btnSave_Click(object sender, EventArgs e)
@@ -42,8 +60,18 @@ namespace SchoolGrades
         /// </summary>
         private void FromUiToClass()
         {
-            currentUser.LastName = txtLastName.Text; 
-            throw new NotImplementedException();
+            currentUser.LastName = txtLastName.Text;
+            currentUser.FirstName = txtName.Text;
+            currentUser.Email = txtEmail.Text;
+            currentUser.Username = txtUsername.Text;
+            currentUser.LastChange = DateTime.Now;
+            currentUser.Description = txtDescription.Text;
+            currentUser.Email = txtEmail.Text;
+            if (chkIsUserEnabled.Checked)
+            {
+                currentUser.IsEnabled = true;
+            }
+            currentUser.IdUserCategory = Convert.ToInt32(txtID.Text);
         }
     }
 }
