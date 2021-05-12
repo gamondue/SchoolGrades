f43872d443bbd32348f56ca82d35ae6f11a7a1f5
Luca Casadei <LUCA.CASADEI@local.ittcesena.it>
2021-05-12 09:49:38 +0200

    Metodi.

---
+++ DataLayer/DL_ImageManagement.cs
@@ -0,0 +1,48 @@
+﻿using SchoolGrades.DbClasses;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+
+namespace SchoolGrades
+{
+    internal partial class DataLayer
+    {
+        internal List<Image> GetAllImagesShownToAClassDuringLessons(Class Class, SchoolSubject Subject,
+            DateTime DateStart = default(DateTime), DateTime DateFinish = default(DateTime))
+        {
+            List<Image> images = new List<Image>();
+
+            DbDataReader dRead;
+            DbCommand cmd;
+            using (DbConnection conn = Connect())
+            {
+                cmd = conn.CreateCommand();
+                string query;
+                query = "SELECT * FROM Images" +
+                        " JOIN Lessons_Images ON Images.idImage=Lessons_Images.idImage" +
+                        " JOIN Lessons ON Lessons.idLesson=Lessons_Images.idLesson" +
+                        " WHERE Lessons.idClass=" + Class.IdClass +
+                        " AND Lessons.idSchoolSubject='" + Subject.IdSchoolSubject + "'";
+                if (DateStart != default(DateTime) && DateFinish != default(DateTime))
+                    query += " AND Lessons.date BETWEEN " +
+                    SqlVal.SqlDate(DateStart) + " AND " + SqlVal.SqlDate(DateFinish);
+                query += ";";
+                cmd.CommandText = query;
+                dRead = cmd.ExecuteReader();
+                while (dRead.Read())
+                {
+                    Image i = new Image();
+                    i.IdImage = (int)dRead["IdImage"];
+                    i.Caption = (string)dRead["Caption"];
+                    i.RelativePathAndFilename = (string)dRead["ImagePath"];
+
+                    images.Add(i);
+                }
+                cmd.Dispose();
+                dRead.Dispose();
+            }
+            return images;
+        }
+    }
+}
