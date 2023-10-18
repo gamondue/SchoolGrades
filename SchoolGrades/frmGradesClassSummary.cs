6e1f28f137469c1e4a3024101327632783bd7690
Squire <115690639+SaraCappelli@users.noreply.github.com>
2023-10-19 00:02:41 +0200

    frmGradesSummary, frmGroups spostamento in BusinessLayer delle parti di codice opportune (#102)
    Co-authored-by: Sara Cappelli <SARA.CAPPELLI@local.ittcesena.it>

--- SchoolGrades/frmGradesClassSummary.cs
+++ SchoolGrades/frmGradesClassSummary.cs
@@ -149,27 +149,12 @@ namespace SchoolGrades
         }
         private void cmbSchoolPeriod_SelectedIndexChanged(object sender, EventArgs e)
         {
-            currentSchoolPeriod = (SchoolPeriod)(cmbSchoolPeriod.SelectedValue); 
-            if (currentSchoolPeriod.IdSchoolPeriodType != "N")
-            {
-                dtpStartPeriod.Value = (DateTime)currentSchoolPeriod.DateStart;
-                dtpEndPeriod.Value = (DateTime)currentSchoolPeriod.DateFinish;
-            }
-            else if (currentSchoolPeriod.IdSchoolPeriod == "month")
-            {
-                dtpStartPeriod.Value = DateTime.Now.AddMonths(-1);
-                dtpEndPeriod.Value = DateTime.Now;
-            } 
-            else if (currentSchoolPeriod.IdSchoolPeriod == "week")
-            {
-                dtpStartPeriod.Value = DateTime.Now.AddDays(-7); 
-                dtpEndPeriod.Value = DateTime.Now;
-            }
-            else if (currentSchoolPeriod.IdSchoolPeriod == "year")
-            {
-                dtpStartPeriod.Value = DateTime.Now.AddYears(-1);
-                dtpEndPeriod.Value = DateTime.Now;
-            }
+            currentSchoolPeriod = (SchoolPeriod)(cmbSchoolPeriod.SelectedValue);
+
+            var res = Commons.bl.CalculateStartAndEndPeriod(currentSchoolPeriod);
+            dtpStartPeriod.Value = res.startPeriod;
+            dtpEndPeriod.Value = res.endPeriod;
+
             RetrieveData(FindCheckedRadioButton());
         }
         private RadioButton FindCheckedRadioButton()
