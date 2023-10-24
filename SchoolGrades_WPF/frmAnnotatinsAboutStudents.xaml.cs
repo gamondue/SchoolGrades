8a80824da4029d00739cb00bb4796877c5a64dc3
matteo.faccetta.stud <matteo.faccetta.stud@ispascalcomandini.it>
2023-10-24 18:09:23 +0200

    Inserite le finestre create da Hu

--- SchoolGrades_WPF/frmAnnotatinsAboutStudents.xaml.cs
+++ SchoolGrades_WPF/frmAnnotatinsAboutStudents.xaml.cs
@@ -10,16 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SchoolGrades_WPF
+namespace SchoolGradesWPF
 {
     /// <summary>
-    /// Logica di interazione per frmAnnotationAboutStudent.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class frmAnnotationAboutStudent : Window
+    public partial class MainWindow : Window
     {
-        public frmAnnotationAboutStudent()
+        public MainWindow()
         {
             InitializeComponent();
         }
