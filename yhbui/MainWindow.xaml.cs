7c67c00cb4ab2c3749593a931b6ca66ae972070a
Diego Ducci <DIEGO.DUCCI@local.ittcesena.it>
2023-10-31 09:18:26 +0100

    ossigeno

--- yhbui/MainWindow.xaml.cs
+++ yhbui/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace yhbui
         {
             InitializeComponent();
         }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
