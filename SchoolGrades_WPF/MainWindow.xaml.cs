83b45712644bb264a2c869a967b6cecc22bf8d1a
Gabriele MONTI <gamondue@gmail.com>
2023-11-01 01:44:12 +0100

    Some windows from class 4E. Adjusted and compiling,
    No code in code behind. UI events stripped from the XAML.

--- SchoolGrades_WPF/MainWindow.xaml.cs
+++ SchoolGrades_WPF/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace SchoolGrades_WPF
 {
@@ -23,21 +10,6 @@ namespace SchoolGrades_WPF
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void CheckBox_Checked(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
--- SchoolGrades_WPF/frmKnotsToTheComb.xaml.cs
+++ SchoolGrades_WPF/frmKnotsToTheComb.xaml.cs
@@ -1,28 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace SchoolGrades_WPF
 {
     /// <summary>
     /// Logica di interazione per frmKnotsToTheComb.xaml
     /// </summary>
-    public partial class frmKnotsToTheComb : Page
+    public partial class frmKnotsToTheComb : Window
     {
         public frmKnotsToTheComb()
         {
             InitializeComponent();
         }
+
     }
 }
