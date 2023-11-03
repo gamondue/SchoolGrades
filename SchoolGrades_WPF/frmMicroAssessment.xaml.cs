a6f61588a383b0f8801b1455a97847ddba727461
Gabriele MONTI <gamondue@gmail.com>
2023-11-03 01:28:19 +0100

    Added some student provided WPF windows. Trasplanted ad adjusted first code that takes data from the Db.

--- SchoolGrades_WPF/frmMicroAssessment.xaml.cs
+++ SchoolGrades_WPF/frmMicroAssessment.xaml.cs
@@ -3,11 +3,11 @@
 namespace SchoolGrades_WPF
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logica di interazione per MicroAssessmentWPF.xaml
     /// </summary>
-    public partial class frmLesson : Window
+    public partial class frmMicroAssessment : Window
     {
-        public frmLesson()
+        public frmMicroAssessment()
         {
             InitializeComponent();
         }
