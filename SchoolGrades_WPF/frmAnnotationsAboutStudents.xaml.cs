b8d8c2a0edd2e2448ff2262aa81d5cd692ff1225
Gabriele MONTI <gamondue@gmail.com>
2023-11-13 23:43:48 +0100

    Added frmAnnotationsAboutStudents, frmRandom, frmStudent

---
+++ SchoolGrades_WPF/frmAnnotationsAboutStudents.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace SchoolGrades_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class frmAnnotationsAboutStudents : Window
+    {
+        public frmAnnotationsAboutStudents()
+        {
+            InitializeComponent();
+        }
+    }
+}
