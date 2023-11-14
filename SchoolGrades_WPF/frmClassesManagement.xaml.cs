5c69b2f80734919851d89703c84ef57b75807610
Mathias Pirazzini <mathias.pirazzini.stud@ispascalcomandini.it>
2023-11-14 12:34:02 +0100

    commit

--- SchoolGrades_WPF/frmClassesManagement.xaml.cs
+++ SchoolGrades_WPF/frmClassesManagement.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace SchoolGrades_WPF
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logica di interazione per frmGestioneClassi.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class frmClassesManagement : Window
     {
-        public MainWindow()
+        public frmClassesManagement()
         {
             InitializeComponent();
         }
--- SchoolGrades_WPF/frmGroups.xaml.cs
+++ SchoolGrades_WPF/frmGroups.xaml.cs
@@ -10,16 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace SchoolGrades_WPF
 {
     /// <summary>
-    /// Logica di interazione per wpfGrade.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class wpfGrade : Window
+    public partial class frmGroups : Window
     {
-        public wpfGrade()
+        public frmGroups()
         {
             InitializeComponent();
         }
