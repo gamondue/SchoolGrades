0dcfaa14b3d54ccde2dcd35d941b7c2646516ab7
Gabriele MONTI <gamondue@gmail.com>
2023-11-22 15:11:46 +0100

    v.0.54.1.11 fixed error when database in configuration was not existing
    Refactorings: moved to BusinessLayer some code from frmMain.
    Refactored GetNewestAmongFilesWithDateInName(), sent to Commons

--- SharedWinForms/CommonsWinForms.cs
+++ SharedWinForms/CommonsWinForms.cs
@@ -289,48 +289,5 @@ namespace SharedWinForms
             }
             return false;
         }
-        internal static void WriteConfigData()
-        {
-            string[] dati = new string[6];
-            try
-            {
-                if (!Directory.Exists(Commons.PathConfig))
-                    Directory.CreateDirectory(Commons.PathConfig);
-                if (!Directory.Exists(Commons.PathLogs))
-                    Directory.CreateDirectory(Commons.PathLogs);
-                if (!Directory.Exists(Commons.PathImages))
-                    Directory.CreateDirectory(Commons.PathImages);
-                //if (!Directory.Exists(Commons.PathStartLinks))
-                //    Directory.CreateDirectory(Commons.PathStartLinks);
-                if (!Directory.Exists(Commons.PathDatabase))
-                    Directory.CreateDirectory(Commons.PathDatabase);
-                if (!Directory.Exists(Commons.PathDocuments))
-                {
-                    if (Commons.PathDocuments != "")
-                        Directory.CreateDirectory(Commons.PathDocuments);
-                    else
-                        Commons.PathDocuments = ".";
-                }
-                dati[0] = Commons.PathAndFileDatabase;
-                dati[1] = Commons.PathImages;
-                //dati[2] = Commons.PathStartLinks;
-                dati[3] = Commons.PathDatabase;
-                dati[4] = Commons.PathDocuments;
-                dati[5] = Commons.SaveBackupWhenExiting.ToString();
-#if DEBUG
-                TextFile.ArrayToFile(Commons.PathAndFileConfig + "_DEBUG", dati, false);
-#else
-                TextFile.ArrayToFile(Commons.PathAndFileConfig, dati, false);
-#endif
-                MessageBox.Show("File di configurazione salvato in " + Commons.PathAndFileConfig);
-            }
-            catch (Exception e)
-            {
-                string err = @"[Error in program's directories] \r\n" + e.Message;
-                Commons.ErrorLog(err);
-                throw new FileNotFoundException(err);
-                //return;
-            }
-        }
     }
 }
