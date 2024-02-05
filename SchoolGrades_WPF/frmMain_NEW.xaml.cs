96356b7de1ec66c0bec62b7f41b45e225ad7d8ad
gamon su Vegetto <gamondue@gmail.com>
2024-02-05 01:43:00 +0100

    Big refactoring of frmMain, almost concluded.

---
+++ SchoolGrades_WPF/frmMain_NEW.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace SchoolGrades_WPF
+{
+    /// <summary>
+    /// Logica di interazione per frmMain.xaml
+    /// </summary>
+    public partial class frmMain_NEW : Window
+    {
+        public frmMain_NEW()
+        {
+            InitializeComponent();
+        }
+
+        private void lstClasses_MouseDoubleClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void lstClasses_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
