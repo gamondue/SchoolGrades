9f1e0a126def8607e5bb02080aebf5d4affd2c1b
gamon su Vegetto <gamondue@gmail.com>
2024-03-06 00:16:04 +0100

    Introduced abstract mother class TreeMpttDb for daughter classes TreeMpttDb_SqLite and TreeMpttDb_SqlServer, that now inherit from TreeMpttDb. Completed and fixed the instantiation and use of TreeMpttDB, in all programs. Fix in Lite_ClassManagement of the conversion to a date (was string), since gave error in import from csv.

--- BusinessObjects/Class.cs
+++ BusinessObjects/Class.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Drawing;
-using gamon;
-
 namespace SchoolGrades.BusinessObjects
 {
     public class Class
@@ -27,25 +21,21 @@ namespace SchoolGrades.BusinessObjects
         public string Description { get; internal set; }
 
         private string pathRestrictedApplication;
-
         public Class()
         {
         }
-
         public Class(string nomeFile)
         {
-            FileName = nomeFile; 
+            FileName = nomeFile;
         }
-
-        public Class(int? IdClass, string Abbreviation, 
+        public Class(int? IdClass, string Abbreviation,
             string SchoolYear, string IdSchool)
         {
             this.IdClass = IdClass;
             this.Abbreviation = Abbreviation;
             this.SchoolYear = SchoolYear;
-            this.IdSchool = IdSchool; 
+            this.IdSchool = IdSchool;
         }
-
         public override string ToString()
         {
             return Abbreviation;
